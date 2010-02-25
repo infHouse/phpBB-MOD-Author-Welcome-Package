c1d820985e8390a41d3aeb5bd41db1dd42577315
obsidian <obsidian@fb0ae243-36e0-4368-812d-0a10cc06066f>
2010-02-25 20:38:32 +0000

    updated the generator, split stuff up a bit and really clarified things.  Should be easier to use now.
    git-svn-id: http://svn6.assembla.com/svn/mod_welcome@32 fb0ae243-36e0-4368-812d-0a10cc06066f

--- develop/generate_readme_file.php
+++ develop/generate_readme_file.php
@@ -16,328 +16,26 @@
  */
 
 // This is all that we will actually run.  Simple, is it not?  :D
+/**
+ * @ignore
+ */
+require './generator_config.php';
 readme_config::init();
 $readme = new readme();
-file_put_contents(readme_config::README_FILE_PATH . 'readme.html', $readme);
+$readme();
+file_put_contents(readme_config::$readme_file_path . 'readme.html', $readme);
 echo 'Done.' . PHP_EOL;
 exit;
 
 /**
- * Readme configuration class
- * Edit this to alter the contents of the readme file that is generated
- */
-class readme_config
-{
-	// Ye olde page title.
-	public static $page_title = '';
-
-	// And a description of the page
-	public static $page_desc = '';
-
-	// Hell, let's do a subtitle.
-	public static $page_subtitle = '';
-
-	// Author's information.
-	// Should be an array containing elements "name", "avatar", and "rank".  Leave stuff blank if you don't want it to appear.
-	public static $author_info = array();
-
-	// Stuff that we...stuff...into the meta tags.
-	public static $meta_info = array();
-
-	// Intro message
-	public static $intro = '';
-
-	// Idiot warnings.  It's not mah fault you broke your shiz.
-	public static $disclaimer = '';
-
-	// And to the meat of the matter...
-	public static $main_data = array();
-
-	// Blah blah blah...stuff at the bottom
-	public static $footer = '';
-
-	// Root paths for style stuff and images
-	const STYLE_ROOT_PATH = 'html/style/';
-	const IMAGE_ROOT_PATH = 'html/images/';
-
-	// The location of the readme file that we'll be dumping to
-	const README_FILE_PATH = './../';
-
-	// If we want this to be cold and impersonal, set to true.
-	const DISABLE_AUTHOR_MODE = false;
-
-	public static function init()
-	{
-		self::$page_title = 'phpBB3 MOD Author Welcome Package';
-		self::$page_desc = 'Open Source at its finest';
-		self::$page_subtitle = 'Building phpBB3 MODs';
-		self::$author_info = array(
-			1 => array(
-				'name' 			=> 'Obsidian',
-				'color'			=> '',
-				'phpbb_com'		=> 'http://www.phpbb.com/community/memberlist.php?mode=viewprofile&amp;u=480595',
-				'avatar'		=> self::IMAGE_ROOT_PATH . 'xkcd_avvy.png',
-				'avatar_wid'	=> 100,
-				'avatar_hei'	=> 100,
-				'rank'			=> 'Jr. MOD Validator',
-			),
-			2 => array(
-				'name' 			=> 'SyntaxError90',
-				'color'			=> '#660099',
-				'phpbb_com'		=> 'http://www.phpbb.com/community/memberlist.php?mode=viewprofile&amp;u=873955',
-				'avatar'		=> self::IMAGE_ROOT_PATH . 'engie_hat.png',
-				'avatar_wid'	=> 100,
-				'avatar_hei'	=> 100,
-				'rank'			=> 'MOD Team Member',
-			),
-		);
-		self::$meta_info = array(
-			'copyright'		=> '2010 Obsidian',
-			'keywords'		=> '',
-			'description'	=> 'phpBB 3.0.x MOD Author Welcome Package',
-			'emulate_ie7'	=> true, // Compatibility mode for that stupid browser that Microsoft makes
-		);
-		self::$intro = 'Hey there, and welcome to the phpBB MOD Author Welcome Package. <br />This guide is intended to help you become a MOD author; within, you will learn how to modify phpBB to suit your needs and the needs of others, and how to do it <em>right</em>.';
-		self::$disclaimer = '';
-		self::$main_data = array(
-			array(
-				'section_title'		=> 'Introduction',
-				'unique_name'		=> 'intro',
-				'contents'			=> array(
-					array(
-						'content' 		=> parse('obsidian_intro.txt'),
-						'author'		=> 1,
-					),
-					array(
-						'content' 		=> parse('sam_intro.txt'),
-						'author'		=> 2,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Coding for phpBB: What you will need',
-				'unique_name'		=> 'overview',
-				'contents'			=> array(
-					array(
-						'content' 		=> parse('needs.txt'),
-						'author'		=> 1,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'What is included within the Welcome Package',
-				'unique_name'		=> 'included_items',
-				'contents'			=> array(
-					array(
-						'content' 		=> parse('included_overview.txt'),
-						'author'		=> 1,
-					),
-					array(
-						'content' 		=> parse('included_automod.txt'),
-						'author'		=> NULL,
-					),
-					array(
-						'content' 		=> parse('included_umil.txt'),
-						'author'		=> NULL,
-					),
-					array(
-						'content' 		=> parse('included_modx.txt'),
-						'author'		=> NULL,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Coding requirements',
-				'unique_name'		=> 'requirements',
-				'contents'			=> array(
-					array(
-						'content' 		=> 'blah',
-						'author'		=> 2,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Coding resources and links',
-				'unique_name'		=> 'resources',
-				'contents'			=> array(
-					array(
-						'content' 		=> 'blah',
-						'author'		=> 1,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Desired coding habits with phpBB',
-				'unique_name'		=> 'habits',
-				'contents'			=> array(
-					array(
-						'content' 		=> 'blah',
-						'author'		=> 2,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Building larger MODs: Step by Step',
-				'unique_name'		=> 'codingprocess',
-				'contents'			=> array(
-					array(
-						'content' 		=> 'blah',
-						'author'		=> 1,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Third party tools for development',
-				'unique_name'		=> 'thirdpartytools',
-				'contents'			=> array(
-					array(
-						'content' 		=> 'blah',
-						'author'		=> 1,
-					),
-				),
-			),
-			array(
-				'section_title'		=> 'Test',
-				'unique_name'		=> 'test',
-				'contents'			=> array(
-					array(
-						'content' 		=> parse('test.txt'),
-						'author'		=> 1,
-					),
-				),
-			),
-		);
-		self::$footer = 'MOD Author Welcome Package &copy; 2010 ' . '<strong>Obsidian</strong>';
-	}
-}
-
-/**
  * =========================================================================
- * DO NOT ALTER ANYTHING BEYOND THIS POINT UNLESS NECESSARY
+ * DO NOT ALTER ANYTHING BEYOND THIS POINT UNLESS ABSOLUTELY NECESSARY
  * =========================================================================
  */
 
 /**
- * The class that generates the readme file.
+ * These functions parse input from the txt files and output the HTML.
  */
-class readme
-{
-	// Revision number for this document
-	protected $revision = '$Id$';
-
-	protected $data = '';
-
-	// Here we build the HTML page.
-	// Get ready for some work.
-	public function __toString()
-	{
-		// This is what we will return.  Append all data to this.
-		// Build the header area.
-		$this->append('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-			<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en" xml:lang="en">
-			<head>
-			<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-			<meta http-equiv="content-style-type" content="text/css" />
-			<meta http-equiv="content-language" content="en" />
-			<meta http-equiv="imagetoolbar" content="no" />
-			<meta name="resource-type" content="document" />
-			<meta name="distribution" content="global" />
-			<meta name="copyright" content="' . readme_config::$meta_info['copyright'] . '" />
-			<meta name="keywords" content="' . readme_config::$meta_info['keywords'] . '" />
-			<meta name="description" content="' . readme_config::$meta_info['description'] . '" />
-			' . ((readme_config::$meta_info['emulate_ie7']) ? '<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />' : '') . '
-			<title>' . readme_config::$page_title . '</title>
-			<link href="' . readme_config::STYLE_ROOT_PATH . 'stylesheet.css" rel="stylesheet" type="text/css" media="screen, projection" />
-			<!-- $Id$ -->
-			</head>
-			<body id="phpbb" class="section-docs">
-			<div id="wrap">
-				<a id="top" name="top" accesskey="t"></a>
-				<div id="page-header">
-					<div class="headerbar">
-						<div class="inner"><span class="corners-top"><span></span></span>
-						<div id="doc-description">
-							<a href="#" id="logo"><img src="' . readme_config::STYLE_ROOT_PATH . 'site_logo.gif" alt="" /></a>
-							<h1>' . readme_config::$page_title . '</h1>
-							<p>' . readme_config::$page_desc . '</p>
-							<p style="display: none;"><a href="#start_here">Skip</a></p>
-						</div>
-						<span class="corners-bottom"><span></span></span></div>
-					</div>
-				</div>
-				<a name="start_here"></a>
-				<div id="page-body">
-			<!-- BEGIN DOCUMENT -->');
-
-		// Let's take a breather...
-
-		$this->append('<p>' . readme_config::$intro . '</p>
-			<h1>' . readme_config::$page_subtitle . '</h1>
-			<div class="paragraph menu">
-				<div class="inner"><span class="corners-top"><span></span></span>
-				<div class="content">
-					<ul>');
-
-		// Build the list of topics that will be discussed
-		foreach(readme_config::$main_data as $row)
-		{
-			$this->append("<li><a href=\"#{$row['unique_name']}\">{$row['section_title']}</a></li>");
-		}
-
-		$this->append('</ul>
-				</div>
-				<span class="corners-bottom"><span></span></span></div>
-			</div>');
-
-		foreach(readme_config::$main_data as $row)
-		{
-			$this->append("<br /><hr />
-				<a name=\"{$row['unique_name']}\"></a><h2>{$row['section_title']}</h2>");
-			foreach($row['contents'] as $c_row)
-			{
-				$this->append('<div class="paragraph">
-					<div class="inner"><span class="corners-top"><span></span></span>
-						<div class="content post" ' . ((readme_config::DISABLE_AUTHOR_MODE === true || $c_row['author'] === NULL) ? 'style="width: 100%;" ' : '') . '>');
-				$this->append(str_replace("\n", "<br />\n", $c_row['content']), true);
-				$this->append('</div>');
-				if(readme_config::DISABLE_AUTHOR_MODE !== true && $c_row['author'] !== NULL)
-				{
-					$this->append('<dl class="postprofile">
-						<dt>
-							<a href="' . readme_config::$author_info[$c_row['author']]['phpbb_com'] . '"><img src="' . readme_config::$author_info[$c_row['author']]['avatar'] . '" width="' . readme_config::$author_info[$c_row['author']]['avatar_wid'] . '" height="' . readme_config::$author_info[$c_row['author']]['avatar_hei'] . '" alt="User avatar" /></a><br />
-							<a href="' . readme_config::$author_info[$c_row['author']]['phpbb_com'] . '" ' . ((readme_config::$author_info[$c_row['author']]['color']) ? 'style="color: ' . readme_config::$author_info[$c_row['author']]['color'] . ';" class="username-coloured"' : '') . '>' . readme_config::$author_info[$c_row['author']]['name'] . '</a>
-						</dt>
-						<dd>' . readme_config::$author_info[$c_row['author']]['rank'] . '</dd>
-					</dl>');
-				}
-				$this->append('<div class="back2top"><a href="#wrap" class="top">Back to Top</a></div>
-					<span class="corners-bottom"><span></span></span></div>
-				</div>');
-			}
-		}
-
-		$this->append('<!-- END DOCUMENT -->
-					<div id="page-footer">
-						<div class="version">' . readme_config::$footer . ' | ' . $this->revision . ' </div>
-					</div>
-				</div></div>
-				<div>
-					<a id="bottom" name="bottom" accesskey="z"></a>
-				</div>
-			</body>
-			</html>');
-		return $this->data;
-	}
-
-	private function append($data, $tabsafe = false)
-	{
-		$data = explode("\n", $data);
-		foreach($data as $row)
-		{
-			$this->data .= ((!$tabsafe) ? trim($row) : $row) . "\n";
-		}
-	}
-}
 
 /**
  * Parses an input txt file for docs
@@ -351,17 +49,11 @@ function parse($filename)
 		echo 'File /txt/' . $filename . ' does not exist' . PHP_EOL . 'Generator failed.' . PHP_EOL;
 		exit;
 	}
-	return _parse(file_get_contents('./txt/' . $filename));
-}
 
-/**
- * Parses fauxcode in input text, then returns it
- * @param string $text - The text to parse.
- * @return string - fauxcode->html parsed string
- */
-function _parse($text = '')
-{
-	// meh
+	// Load the file
+	$text = file_get_contents('./txt/' . $filename);
+
+	// Build our array of PCRE patterns and their replacements
 	$fauxcode = array(
 		// bold
 		"#\[b\](.*?)\[/b\]#is" => '<span style="font-weight: bold">$1</span>',
@@ -390,4 +82,131 @@ function _parse($text = '')
 	);
 
 	return preg_replace(array_keys($fauxcode), array_values($fauxcode), $text);
+}
+
+/**
+ * The class that generates the readme file.
+ */
+class readme
+{
+	// Revision number for this document
+	protected $revision = '$Id$';
+
+	protected $data = '';
+
+	// Here we build the HTML page.
+	// Get ready for some work.
+	public function __invoke()
+	{
+		// This is what we will return.  Append all data to this.
+		// Build the header area.
+		$this->append('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+			<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en" xml:lang="en">
+			<head>
+			<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+			<meta http-equiv="content-style-type" content="text/css" />
+			<meta http-equiv="content-language" content="en" />
+			<meta http-equiv="imagetoolbar" content="no" />
+			<meta name="resource-type" content="document" />
+			<meta name="distribution" content="global" />
+			<meta name="copyright" content="' . readme_config::$meta_info['copyright'] . '" />
+			<meta name="keywords" content="' . readme_config::$meta_info['keywords'] . '" />
+			<meta name="description" content="' . readme_config::$meta_info['description'] . '" />
+			' . ((readme_config::$meta_info['emulate_ie7']) ? '<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />' : '') . '
+			<title>' . readme_config::$page_title . '</title>
+			<link href="' . readme_config::$style_root_path . 'stylesheet.css" rel="stylesheet" type="text/css" media="screen, projection" />
+			<!-- $Id$ -->
+			</head>
+			<body id="phpbb" class="section-docs">
+			<div id="wrap">
+				<a id="top" name="top" accesskey="t"></a>
+				<div id="page-header">
+					<div class="headerbar">
+						<div class="inner"><span class="corners-top"><span></span></span>
+						<div id="doc-description">
+							<a href="#" id="logo"><img src="' . readme_config::$style_root_path . 'site_logo.gif" alt="" /></a>
+							<h1>' . readme_config::$page_title . '</h1>
+							<p>' . readme_config::$page_desc . '</p>
+							<p style="display: none;"><a href="#start_here">Skip</a></p>
+						</div>
+						<span class="corners-bottom"><span></span></span></div>
+					</div>
+				</div>
+				<a name="start_here"></a>
+				<div id="page-body">
+			<!-- BEGIN DOCUMENT -->');
+
+		// Let's take a breather...
+
+		$this->append('<p>' . readme_config::$intro . '</p>
+			<h1>' . readme_config::$page_subtitle . '</h1>
+			<div class="paragraph menu">
+				<div class="inner"><span class="corners-top"><span></span></span>
+				<div class="content">
+					<ul>');
+
+		// Build the list of topics that will be discussed
+		foreach(readme_config::$main_data as $key => $row)
+		{
+			$this->append("<li><a href='#" . md5($row['section_title'] . $key) . "'>{$row['section_title']}</a></li>");
+		}
+
+		$this->append('</ul>
+				</div>
+				<span class="corners-bottom"><span></span></span></div>
+			</div>');
+
+		foreach(readme_config::$main_data as $key => $row)
+		{
+			$this->append("<br /><hr />
+				<a name='" . md5($row['section_title'] . $key) . "'></a><h2>{$row['section_title']}</h2>");
+			foreach($row['contents'] as $c_row)
+			{
+				$this->append('<div class="paragraph">
+					<div class="inner"><span class="corners-top"><span></span></span>
+						<div class="content post" ' . ((readme_config::$disable_author_mode == true || $c_row['author'] === NULL) ? 'style="width: 100%;" ' : '') . '>');
+				$this->append(str_replace("\n", "<br />\n", $c_row['content']), true);
+				$this->append('</div>');
+				if(readme_config::$disable_author_mode != true && $c_row['author'] !== NULL)
+				{
+					$this->append('<dl class="postprofile">
+						<dt>
+							<a href="' . readme_config::$author_info[$c_row['author']]['phpbb_com'] . '"><img src="' . readme_config::$image_root_path . readme_config::$author_info[$c_row['author']]['avatar'] . '" width="' . readme_config::$author_info[$c_row['author']]['avatar_wid'] . '" height="' . readme_config::$author_info[$c_row['author']]['avatar_hei'] . '" alt="User avatar" /></a><br />
+							<a href="' . readme_config::$author_info[$c_row['author']]['phpbb_com'] . '" ' . ((readme_config::$author_info[$c_row['author']]['color']) ? 'style="color: ' . readme_config::$author_info[$c_row['author']]['color'] . ';" class="username-coloured"' : '') . '>' . readme_config::$author_info[$c_row['author']]['name'] . '</a>
+						</dt>
+						<dd>' . readme_config::$author_info[$c_row['author']]['rank'] . '</dd>
+					</dl>');
+				}
+				$this->append('<div class="back2top"><a href="#wrap" class="top">Back to Top</a></div>
+					<span class="corners-bottom"><span></span></span></div>
+				</div>');
+			}
+		}
+
+		$this->append('<!-- END DOCUMENT -->
+					<div id="page-footer">
+						<div class="version">' . readme_config::$footer . ' | ' . $this->revision . ' </div>
+					</div>
+				</div></div>
+				<div>
+					<a id="bottom" name="bottom" accesskey="z"></a>
+				</div>
+			</body>
+			</html>');
+	}
+
+	// We output the HTML now.
+	public function __toString()
+	{
+		return $this->data;
+	}
+
+	private function append($data, $tabsafe = false)
+	{
+		$data = explode("\n", $data);
+		foreach($data as $row)
+		{
+			$this->data .= ((!$tabsafe) ? trim($row) : $row) . "\n";
+		}
+	}
 }
---
+++ develop/generator_config.php
@@ -0,0 +1,272 @@
+<?php
+
+/**
+ * Readme configuration class
+ * Edit this to alter the contents of the readme file that is generated
+ */
+class readme_config
+{
+	/**
+	 * @var string - The page's title
+	 */
+	public static $page_title = '';
+
+	/**
+	 * @var string - A description of the page.
+	 */
+	public static $page_desc = '';
+
+	/**
+	 * @var string - A subtitle for the page if we want one.
+	 */
+	public static $page_subtitle = '';
+
+	/**
+	 * @var array - Array of author's information.  See below for the format.  (anything you don't want to appear, leave empty/blank/0/null)
+	 */
+	public static $author_info = array();
+
+	/**
+	 * @ignore
+	 *
+	 * 	self::$author_info = array(
+	 * 		// Here we assign an author their ID, and begin defining their information
+	 * 		1 => array(
+	 *			// Your username goes here.
+	 *			'name' 			=> 'username',
+	 *			// A rank color, if you have one
+	 *			'color'			=> '',
+	 *			// A link to your phpBB.com profile, if you wish to link to it.
+	 *			'phpbb_com'		=> 'http://www.phpbb.com/community/memberlist.php?mode=viewprofile&amp;u=480595',
+	 *			// The location of your avatar, if you have one.  Note that the var readme_config::$image_root_path will automatically be appended.
+	 *			'avatar'		=> 'xkcd_avvy.png',
+	 *			// The width of your avatar, if you have one.
+	 *			'avatar_wid'	=> 100,
+	 *			// The height of your avatar, if you have one.
+	 *			'avatar_hei'	=> 100,
+	 *			// A rank title, if you have one.
+	 *			'rank'			=> 'Jr. MOD Validator',
+	 *		),
+	 *		// You can repeat the previous array as many times as necessary.
+	 *	);
+	 *
+	 */
+
+	/**
+	 * @var array - Stuff that we want put into the page's meta tags.
+	 */
+	public static $meta_info = array();
+
+	/**
+	 * @var string - The intro message for this page.
+	 */
+	public static $intro = '';
+
+	/**
+	 * @var string - A disclaimer for your readme file, just in case you want one.
+	 */
+	public static $disclaimer = '';
+
+	/**
+	 * @var array - The meat of the matter; the content of the readme.  Format is below.
+	 */
+	public static $main_data = array();
+
+	/**
+	 * @ignore
+	 *
+	 * 	self::$main_data = array(
+	 * 		// Here we begin defining a section.
+	 * 		array(
+	 * 			// This is the title of the section. It is often best to make this unique from every other section, but not required.
+	 *			'section_title'		=> 'A Random Test Section',
+	 *			// Here we define the contents of the section
+	 *			'contents'			=> array(
+	 *				// Here is a chunk of content that will appear on the readme.
+	 *				array(
+	 *					// This is the actual content.  Content should be stored in a fauxcode-formatted text file in the develop/txt/ directory, and end with the .txt file extension.
+	 *					'content' 		=> parse('some_txt_file.txt'),
+	 * 					// This is who will appear as the "author" of the content.  The ID number used should be that of one assigned previously in readme_config::$author_info.  If you want a specific section to appear to have no author, use NULL instead of an author ID.
+	 *					'author'		=> 1,
+	 *				),
+	 *				// You can repeat the previous array as many times as necessary.
+	 *			),
+	 *		),
+	 *		// You can repeat the previous array as many times as necessary.
+	 * 	);
+	 *
+	 */
+
+	/**
+	 * @var string - The footer for the page
+	 */
+	public static $footer = '';
+
+	/**
+	 * @var string - Root path for style/CSS related HTML includes
+	 */
+	public static $style_root_path = '';
+
+	/**
+	 * @var string - Root path for included images, such as avatars and screenshots.
+	 */
+	public static $image_root_path = '';
+
+	/**
+	 * @var string - The location of the readme file that we'll be dumping the generated content to
+	 */
+	public static $readme_file_path = '';
+
+	/**
+	 * @var boolean - Should we forcibly disable the display of authors for the entire readme?
+	 */
+	public static $disable_author_mode = false;
+
+	public static function init()
+	{
+		// We'll set our various filepaths first.
+		self::$readme_file_path = './../';
+		self::$image_root_path = 'html/images/';
+		self::$style_root_path = 'html/style/';
+
+		// Now, for titles, descriptions, and that stuff.
+		self::$page_title = 'phpBB3 MOD Author Welcome Package';
+		self::$page_desc = 'Open Source at its finest';
+		self::$page_subtitle = 'Building phpBB3 MODs';
+		self::$meta_info = array(
+			'copyright'		=> '2010 Obsidian',
+			'keywords'		=> '',
+			'description'	=> 'phpBB 3.0.x MOD Author Welcome Package',
+			'emulate_ie7'	=> true, // Compatibility mode for that stupid browser that Microsoft makes
+		);
+
+		// The information about the author(s), and the ability to not need info about them.
+		self::$disable_author_mode = false;
+		self::$author_info = array(
+			1 => array(
+				'name' 			=> 'Obsidian',
+				'color'			=> '',
+				'phpbb_com'		=> 'http://www.phpbb.com/community/memberlist.php?mode=viewprofile&amp;u=480595',
+				'avatar'		=> 'xkcd_avvy.png',
+				'avatar_wid'	=> 100,
+				'avatar_hei'	=> 100,
+				'rank'			=> 'Jr. MOD Validator',
+			),
+			2 => array(
+				'name' 			=> 'SyntaxError90',
+				'color'			=> '#660099',
+				'phpbb_com'		=> 'http://www.phpbb.com/community/memberlist.php?mode=viewprofile&amp;u=873955',
+				'avatar'		=> 'engie_hat.png',
+				'avatar_wid'	=> 100,
+				'avatar_hei'	=> 100,
+				'rank'			=> 'MOD Team Member',
+			),
+		);
+
+		// Some basic content.
+		self::$intro = 'Hey there, and welcome to the phpBB MOD Author Welcome Package. <br />This guide is intended to help you become a MOD author; within, you will learn how to modify phpBB to suit your needs and the needs of others, and how to do it <em>right</em>.';
+		self::$disclaimer = '';
+		self::$footer = 'MOD Author Welcome Package &copy; 2010 ' . '<strong>Obsidian</strong>';
+
+		// The CONTENT.
+		self::$main_data = array(
+			array(
+				'section_title'		=> 'Introduction',
+				'contents'			=> array(
+					array(
+						'content' 		=> parse('obsidian_intro.txt'),
+						'author'		=> 1,
+					),
+					array(
+						'content' 		=> parse('sam_intro.txt'),
+						'author'		=> 2,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Coding for phpBB: What you will need',
+				'contents'			=> array(
+					array(
+						'content' 		=> parse('needs.txt'),
+						'author'		=> 1,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'What is included within the Welcome Package',
+				'contents'			=> array(
+					array(
+						'content' 		=> parse('included_overview.txt'),
+						'author'		=> 1,
+					),
+					array(
+						'content' 		=> parse('included_automod.txt'),
+						'author'		=> NULL,
+					),
+					array(
+						'content' 		=> parse('included_umil.txt'),
+						'author'		=> NULL,
+					),
+					array(
+						'content' 		=> parse('included_modx.txt'),
+						'author'		=> NULL,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Coding requirements',
+				'contents'			=> array(
+					array(
+						'content' 		=> 'blah',
+						'author'		=> 2,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Coding resources and links',
+				'contents'			=> array(
+					array(
+						'content' 		=> 'blah',
+						'author'		=> 1,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Desired coding habits with phpBB',
+				'contents'			=> array(
+					array(
+						'content' 		=> 'blah',
+						'author'		=> 2,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Building larger MODs: Step by Step',
+				'contents'			=> array(
+					array(
+						'content' 		=> 'blah',
+						'author'		=> 1,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Third party tools for development',
+				'contents'			=> array(
+					array(
+						'content' 		=> 'blah',
+						'author'		=> 1,
+					),
+				),
+			),
+			array(
+				'section_title'		=> 'Test',
+				'contents'			=> array(
+					array(
+						'content' 		=> parse('test.txt'),
+						'author'		=> 1,
+					),
+				),
+			),
+		);
+	}
+}
